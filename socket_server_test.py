ed2674e8965ed422574d86e3870ba3351dffb2b6
hyperloop <hyperloop>
2016-04-27 04:22:11 +0000

    server with infinite loopç

--- socket_server_test.py
+++ socket_server_test.py
@@ -3,19 +3,19 @@ from time import sleep
 s = socket.socket()
 address = ''
 port = 9000
+s.bind((address, port))
 for x in range(0,10):
 	try:
-		s.bind((address, port))
 		s.listen(1)
 		conn, addr = s.accept()
 		while True:
-			data= conn.recv(1).decode("ascii")
+			data=conn.recv(1).decode("ascii")
 			if data:
 				print(data)
 			else:
 				sleep(1)
-		s.close()
 	except Exception as e: 
 		print("something's wrong with %s:%d. Exception is %s" % (address, port, e))
-	finally:
-		s.close()
+#	finally:
+#		s.close()
+s.close()
